4ae998e290a62c2e39e63fa9c64893168ae2d969
weihu <1183969348@qq.com>
2019-09-22 14:47:33 +0800

    button,input组件和测试

---
+++ src/icon.vue
@@ -0,0 +1,18 @@
+<template>
+    <svg class="g-icon">
+        <use :xlink:href="`#i-${name}`"></use>
+        <!--         $ 是模板字符串插值 -->
+    </svg>
+</template>
+<script>
+    import './svg'
+    export default {
+        props:['name']
+    }
+</script>
+<style lang="scss">
+    .g-icon {
+        width: 1em;
+        height: 1em;
+    }
+    </style>
