a3bfaa0153c87fa71c4404da2f14a72d1261e65b
weihu <1183969348@qq.com>
2019-10-19 20:53:03 +0800

    完成基本样式

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="cascader">
-    <div class="trigger" @click="popovervisible = !popovervisible"></div>
+    <div class="trigger" @click="popovervisible = !popovervisible">
+       {{result || '&nbsp;'}}
+    </div>
     <div class="popover-wrapper" v-if="popovervisible">
       <cascader-items
         :items="source"
@@ -25,79 +27,84 @@ export default {
     popoverHeight: {
       type: String
     },
-    selected:{
-      type:Array,
-      default:()=>{
-        return []
+    selected: {
+      type: Array,
+      default: () => {
+        return [];
       }
     }
   },
   data() {
     return {
       popovervisible: false
+    };
+  },
+  methods: {
+    // open () {
+    //   this.popoverVisible = true
+    // },
+    // close () {
+    //   this.popoverVisible = false
+    // },
+    // toggle () {
+    //   if (this.popoverVisible === true) {
+    //     this.close()
+    //   } else {
+    //     this.open()
+    //   }
+    // },
+    onUpdateSelected(newSelected) {
+      this.$emit("update:selected", newSelected);
+      // let lastItem = newSelected[newSelected.length - 1]
+      // let simplest = (children, id) => {
+      //   return children.filter(item => item.id === id)[0]
+      // }
+      // let complex = (children, id) => {
+      //   let noChildren = []
+      //   let hasChildren = []
+      //   children.forEach(item => {
+      //     if (item.children) {
+      //       hasChildren.push(item)
+      //     } else {
+      //       noChildren.push(item)
+      //     }
+      //   })
+      //   let found = simplest(noChildren, id)
+      //   if (found) {
+      //     return found
+      //   } else {
+      //     found = simplest(hasChildren, id)
+      //     if (found) { return found }
+      //     else {
+      //       for (let i = 0; i < hasChildren.length; i++) {
+      //         found = complex(hasChildren[i].children, id)
+      //         if (found) {
+      //           return found
+      //         }
+      //       }
+      //       return undefined
+      //     }
+      //   }
+      // }
+      // let updateSource = (result) => {
+      //   this.loadingItem = {}
+      //   let copy = JSON.parse(JSON.stringify(this.source))
+      //   let toUpdate = complex(copy, lastItem.id)
+      //   toUpdate.children = result
+      //   this.$emit('update:source', copy)
+      // }
+      // if (!lastItem.isLeaf && this.loadData) {
+      //   this.loadData(lastItem, updateSource) // 回调:把别人传给我的函数调用一下
+      //   // 调回调的时候传一个函数,这个函数理论应该被调用
+      //   this.loadingItem = lastItem
+      // }
     }
   },
-    methods: {
-      // open () {
-      //   this.popoverVisible = true
-      // },
-      // close () {
-      //   this.popoverVisible = false
-      // },
-      // toggle () {
-      //   if (this.popoverVisible === true) {
-      //     this.close()
-      //   } else {
-      //     this.open()
-      //   }
-      // },
-      onUpdateSelected (newSelected) {
-        this.$emit('update:selected', newSelected)
-        // let lastItem = newSelected[newSelected.length - 1]
-        // let simplest = (children, id) => {
-        //   return children.filter(item => item.id === id)[0]
-        // }
-        // let complex = (children, id) => {
-        //   let noChildren = []
-        //   let hasChildren = []
-        //   children.forEach(item => {
-        //     if (item.children) {
-        //       hasChildren.push(item)
-        //     } else {
-        //       noChildren.push(item)
-        //     }
-        //   })
-        //   let found = simplest(noChildren, id)
-        //   if (found) {
-        //     return found
-        //   } else {
-        //     found = simplest(hasChildren, id)
-        //     if (found) { return found }
-        //     else {
-        //       for (let i = 0; i < hasChildren.length; i++) {
-        //         found = complex(hasChildren[i].children, id)
-        //         if (found) {
-        //           return found
-        //         }
-        //       }
-        //       return undefined
-        //     }
-        //   }
-        // }
-        // let updateSource = (result) => {
-        //   this.loadingItem = {}
-        //   let copy = JSON.parse(JSON.stringify(this.source))
-        //   let toUpdate = complex(copy, lastItem.id)
-        //   toUpdate.children = result
-        //   this.$emit('update:source', copy)
-        // }
-        // if (!lastItem.isLeaf && this.loadData) {
-        //   this.loadData(lastItem, updateSource) // 回调:把别人传给我的函数调用一下
-        //   // 调回调的时候传一个函数,这个函数理论应该被调用
-        //   this.loadingItem = lastItem
-        // }
-      }
-    },
+  computed: {
+    result() {
+      return this.selected.map(item => item.name).join("/");
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -105,9 +112,14 @@ export default {
 .cascader {
   position: relative;
   .trigger {
-    border: 1px solid black;
-    height: 32px;
-    width: 100px;
+    background: white;
+    height: $input-height;
+    display: inline-flex;
+    align-items: center;
+    padding: 0 1em;
+    min-width: 10em;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
   }
   .popover-wrapper {
     position: absolute;
@@ -116,6 +128,7 @@ export default {
     background: white;
     @extend .box-shadow;
     display: flex;
+    margin-top: 8px;
   }
 }
 </style> 
