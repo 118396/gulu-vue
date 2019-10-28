55d3ebe174e20e5db27bc115a4aac897b3a69da8
weihu <1183969348@qq.com>
2019-10-28 21:04:49 +0800

    基本完成 pager

--- src/demo.vue
+++ src/demo.vue
@@ -1,70 +1,29 @@
 <template>
-  <div>
- <div class="form-wrapper">
-     <form class="form" @submit.prevent="onSubmit">
-       <h1>登录</h1>
-       <demo-form-row label="邮箱" :error="errors.email">
-         <g-input type="text" v-model="user.email"></g-input>
-       </demo-form-row>
-       <demo-form-row label="密码" :error="errors.password">
-         <g-input type="password" v-model="user.password"></g-input>
-       </demo-form-row>
-       <div>
-         <g-button class="ok" type="submit">提交</g-button>
-       </div>
-     </form>
+  <div style="margin: 20px;">
+    <g-pager :total-page="20" :current-page.sync="currentPage"></g-pager>
   </div>
 </template>
 <script>
-import GButton from "./button/button";
-import GInput from "./input";
-import DemoFormRow from "./demo-form-row";
-import formMixin from "./form-mixin";
+import GPager from "./pager";
 
 export default {
   name: "demo",
-  components: { GButton, GInput, DemoFormRow },
-  mixins: [formMixin],
+  components: { GPager },
   data() {
     return {
-      user: {
-        email: "",
-        password: ""
-      },
-      rules: [
-        { key: "email", pattern: "email", required: true },
-        { key: "password", minLength: 6, required: true }
-      ]
+      currentPage: 1
     };
-  },
-  methods: {
-    onSubmit() {
-      this.validate(this.user);
-      console.log(this.errors);
-    }
   }
 };
 </script>
-<style>
-body {
-  background: #888;
-}
-</style>
+
  <style scoped lang="scss">
-.form {
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
   background: white;
-  padding: 24px;
-  border-radius: 8px;
-  margin-top: 36px;
-  min-height: 60vh;
-  &-wrapper {
-    display: flex;
-    justify-content: center;
-  }
-  .ok {
-    display: block;
-    width: 100%;
-    margin-top: 24px;
-  }
 }
 </style>
