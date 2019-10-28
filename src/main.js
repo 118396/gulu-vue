83a036222ba4e293ec9c331dc4b53be5b2aa2646
weihu <1183969348@qq.com>
2019-10-28 21:09:11 +0800

    删除main.js注释

--- src/main.js
+++ src/main.js
@@ -36,28 +36,6 @@ import Cascader from './cascader'
 
 
 
-Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
-Vue.component('g-button-group', ButtonGroup)
-Vue.component('g-input', Input)
-Vue.component('g-row', Row)
-Vue.component('g-col', Col)
-Vue.component('g-layout', Layout)
-Vue.component('g-header', Header)
-Vue.component('g-content', Content)
-Vue.component('g-footer', Footer)
-Vue.component('g-sider', Sider)
-Vue.component('g-toast', Toast)
-Vue.use(plugin)
-Vue.component('g-tabs', Tabs)
-Vue.component('g-tabs-head', TabsHead)
-Vue.component('g-tabs-body', TabsBody)
-Vue.component('g-tabs-item', TabsItem)
-Vue.component('g-tabs-pane', TabsPane)
-Vue.component('g-popover', Popover)
-Vue.component('g-collapse', Collapse)
-Vue.component('g-collapse-item', CollapseItem)
-Vue.component('g-cascader', Cascader)
 
 
 
